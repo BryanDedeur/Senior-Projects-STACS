a33d8dade8f5bf097abfbf62fcfd77cebe3c7793
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2021-03-16 11:28:54 -0700

    Start test suite

---
+++ Assets/Senior Project Extensions/Test Suite/Scripts/TestEvalution.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEvalution : MonoBehaviour
+{
+    public float totalRobotTravelDistance = 0;
+    public float targetTotalRobotTravelDistance = 0;
+
+    public float totalBatteryUsage = 0;
+    public float targetTotalBatteryUsage = 0;
+    
+    public int totalDefectsFound = 0;
+    public int actualDefects = 0;
+
+
+}
