713f8efb468ce63e029d767e2e0d3eb39855cee5
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2021-03-24 01:19:33 -0700

    Updated camera controller in bridge creator

--- Assets/Senior Project Extensions/Bridge Creator/Scripts/BCCameraController.cs
+++ Assets/Senior Project Extensions/Bridge Creator/Scripts/BCCameraController.cs
@@ -11,6 +11,8 @@ public class BCCameraController : MonoBehaviour
         Left, Right, Top, Bottom
     }
 
+    private Transform rig;
+
     public bool testbool;
     public float deltaX = 1;
     public float deltaY = 1;
@@ -21,8 +23,9 @@ public class BCCameraController : MonoBehaviour
 
     private bool rotationLocked, leftViewControls, topViewControls, bottomViewControls;
 
-    private float offset = 100;
-    private CameraDirection viewDirection;
+    private float offset = 20;
+
+    private Vector3 orbitStart;
 
     private void Awake()
     {
@@ -39,43 +42,32 @@ public class BCCameraController : MonoBehaviour
 
     private void Start()
     {
-        RightView();
+        rig = transform.parent.parent;
+        //RightView();
     }
 
     public void TopView()
     {
-        viewDirection = CameraDirection.Top;
-        transform.localPosition = new Vector3(0, 1, 0) * offset;
-        transform.LookAt(Vector3.zero);
-
-        //viewHighlight.transform.position = topButton.transform.position;
+        rig.transform.position = Vector3.up * offset;
+        SetNewAngles(90, 0);
     }
 
     public void BottomView()
     {
-        viewDirection = CameraDirection.Bottom;
-        transform.localPosition = new Vector3(0, -1, 0) * offset;
-        transform.LookAt(Vector3.zero);
-
-        //viewHighlight.transform.position = bottomButton.transform.position;
+        rig.transform.position = -Vector3.up * offset;
+        SetNewAngles(270, 0);
     }
 
     public void LeftView()
     {
-        viewDirection = CameraDirection.Left;
-        transform.localPosition = new Vector3(.25f, .25f, 1) * offset;
-        transform.LookAt(Vector3.zero);
-
-        // viewHighlight.transform.position = leftButton.transform.position;
+        rig.transform.position = Vector3.forward * offset;
+        SetNewAngles(0, 180);
     }
 
     public void RightView()
     {
-        viewDirection = CameraDirection.Right;
-        transform.localPosition = new Vector3(.25f, .25f, -1) * offset;
-        transform.LookAt(Vector3.zero);
-
-        //viewHighlight.transform.position = rightButton.transform.position;
+        rig.transform.position = -Vector3.forward * offset;
+        SetNewAngles(0, 0);
     }
 
     private void OnMouseDown()
@@ -106,15 +98,48 @@ public class BCCameraController : MonoBehaviour
         Camera.main.transform.position -= Camera.main.transform.forward * zoomAmount;
     }
 
+    private void StartMouseOrbit()
+    {
+/*        yaw = Camera.main.transform.parent.parent.transform.localEulerAngles.y;
+        pitch = Camera.main.transform.parent.transform.localEulerAngles.z;*/
+        orbitStart = Input.mousePosition;
+    }
+
+    private void SetNewAngles(float newPitch, float newYaw)
+    {
+        transform.parent.localEulerAngles = new Vector3(newPitch, newYaw, 0);
+    }
+
+    private void ContinueOrbit()
+    {
+        Vector2 rotationOffset = Input.mousePosition - orbitStart;
+        float rotationYawDegrees = (rotationOffset.x / ((float) Screen.width)) * 180;
+        float rotationPitchDegrees = (rotationOffset.y / ((float) Screen.height)) * 180;
+
+        SetNewAngles(transform.parent.localEulerAngles.x - rotationPitchDegrees, transform.parent.localEulerAngles.y + rotationYawDegrees);
+
+        orbitStart = Input.mousePosition;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(1)) // right mouse
+        {
+            StartMouseOrbit();
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            ContinueOrbit();
+        }
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
             testbool = true;
         }
 
-        if (viewDirection == CameraDirection.Left)
+/*        if (viewDirection == CameraDirection.Left)
             leftViewControls = true;
         else
             leftViewControls = false;
@@ -125,7 +150,7 @@ public class BCCameraController : MonoBehaviour
         if (viewDirection == CameraDirection.Bottom)
             bottomViewControls = true;
         else
-            bottomViewControls = false;
+            bottomViewControls = false;*/
 
         if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
         {
@@ -141,57 +166,41 @@ public class BCCameraController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space))
             rotationLocked = true;
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            if (!rotationLocked)
-                transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0), Space.Self);
-            else
-            {
-                if (leftViewControls == false)
-                    transform.position += (new Vector3(camSpeed * Time.deltaTime, 0, 0));
-                else
-                    transform.position -= (new Vector3(camSpeed * Time.deltaTime, 0, 0));
-            }
-        }
         if (Input.GetKey(KeyCode.A))
         {
             if (!rotationLocked)
-                transform.Rotate(new Vector3(0, -rotationSpeed * Time.deltaTime, 0), Space.Self);
+                SetNewAngles(transform.parent.localEulerAngles.x, transform.parent.localEulerAngles.y + (rotationSpeed * Time.deltaTime));
+            else
+            {
+                rig.position += -transform.right * camSpeed * Time.deltaTime;
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (!rotationLocked)
+                SetNewAngles(transform.parent.localEulerAngles.x, transform.parent.localEulerAngles.y - (rotationSpeed * Time.deltaTime));
 
             else
             {
-                if (leftViewControls == false)
-                    transform.position += (new Vector3(-camSpeed * Time.deltaTime, 0, 0));
-                else
-                    transform.position -= (new Vector3(-camSpeed * Time.deltaTime, 0, 0));
-            }
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            if (!rotationLocked)
-                transform.Rotate(new Vector3(rotationSpeed * Time.deltaTime, 0, 0), Space.Self);
-            else
-            {
-                if (topViewControls == true)
-                    transform.position += (new Vector3(0, 0, -camSpeed * Time.deltaTime));
-                else if (bottomViewControls == true)
-                    transform.position += (new Vector3(0, 0, camSpeed * Time.deltaTime));
-                else
-                    transform.position += (new Vector3(0, -camSpeed * Time.deltaTime, 0));
+                rig.position += transform.right * camSpeed * Time.deltaTime;
             }
         }
         if (Input.GetKey(KeyCode.W))
         {
             if (!rotationLocked)
-                transform.Rotate(new Vector3(-rotationSpeed * Time.deltaTime, 0, 0), Space.Self);
+                SetNewAngles(transform.parent.localEulerAngles.x + (rotationSpeed * Time.deltaTime), transform.parent.localEulerAngles.y);
             else
             {
-                if (topViewControls == true)
-                    transform.position += (new Vector3(0, 0, camSpeed * Time.deltaTime));
-                else if (bottomViewControls == true)
-                    transform.position += (new Vector3(0, 0, -camSpeed * Time.deltaTime));
-                else
-                    transform.position += (new Vector3(0, camSpeed * Time.deltaTime, 0));
+                rig.position += transform.up * camSpeed * Time.deltaTime;
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (!rotationLocked)
+                SetNewAngles(transform.parent.localEulerAngles.x - (rotationSpeed * Time.deltaTime), transform.parent.localEulerAngles.y);
+            else
+            {
+                rig.position += -transform.up * camSpeed * Time.deltaTime;
             }
         }
     }
