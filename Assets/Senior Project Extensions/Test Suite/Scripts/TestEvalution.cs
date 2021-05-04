3614318ef00fecaa5cea7e39d999069282adb9a4
Andrew Wiltberger <andrewwiltberger@nevada.unr.edu>
2021-05-04 14:48:34 -0700

    documentation testevaluation script

--- Assets/Senior Project Extensions/Test Suite/Scripts/TestEvalution.cs
+++ Assets/Senior Project Extensions/Test Suite/Scripts/TestEvalution.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// a class that contains the functionallity for scripted test scences 
+/// </summary>
 public class TestEvalution : MonoBehaviour
 {
     public static TestEvalution inst;
@@ -29,6 +33,9 @@ public class TestEvalution : MonoBehaviour
 
     public string resultsPath = "Results/results.txt";
 
+    /// <summary>
+    /// this function tracks the robots sttarting positione 
+    /// </summary>
     private void Awake()
     {
         inst = this;
@@ -39,6 +46,12 @@ public class TestEvalution : MonoBehaviour
             trackedRobotPreviousPos.Add(robot.transform.position);
         }
     }
+
+    /// <summary>
+    /// calculates the total distance traved of all climbing robots 
+    /// and the percent over the target distance traveled if the robots when over the target 
+    /// </summary>
+    /// <returns></returns> a float containg the percent grade for this section 
     public float DistanceError()//return grade for this section
     {
         float difference = totalRobotTravelDistance - targetTotalRobotTravelDistance; //amount over target
@@ -53,6 +66,11 @@ public class TestEvalution : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// calculates the total battery used of all climbing robots 
+    /// and the percent over the target battery usage if the robots when over the target
+    /// </summary>
+    /// <returns></returns> a float containg the percent grade for this section 
     public float BatteryError()//return grade for this section
     {
         float difference = totalBatteryUsage - targetTotalBatteryUsage; //positive num if acutal batts used greater than target
@@ -67,6 +85,10 @@ public class TestEvalution : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// calulates the grade for defects based on amount of defects found 
+    /// </summary>
+    /// <returns></returns> grade for this section
     public float DefectsError()
     {
         if (totalDefectsFound > 0)
@@ -76,6 +98,10 @@ public class TestEvalution : MonoBehaviour
         return 0;
     }
 
+    /// <summary>
+    /// calulates the final grade for the given test based on time taken
+    /// </summary>
+    /// <returns></returns> the final grade to be stored in the results page 
     public float FinalGrade()
     {
         /* float distance = DistanceError();
@@ -215,6 +241,9 @@ public class TestEvalution : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// updates the totalRobotTravelDistance for all tracked robots  
+    /// </summary>
     public void UpdateRobotPositionTracking()
     {
         for (int i = 0; i < trackedRobots.Count; i++)
@@ -224,6 +253,11 @@ public class TestEvalution : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// saves the results to a file 
+    /// pauses the game and shows the results to user,
+    ///    
+    /// /// </summary>
     public void EndTest()
     {
         testEnded = true;
@@ -236,6 +270,9 @@ public class TestEvalution : MonoBehaviour
         UIMgr.inst.ResumeGame();
     }
 
+    /// <summary>
+    /// display the fail test ui and write a failed test to results 
+    /// </summary>
     public void FailTest()
     {
         Debug.Log("Fail Test");
@@ -250,7 +287,10 @@ public class TestEvalution : MonoBehaviour
 
  
 
-
+    /// <summary>
+    /// sets proper strings bases on the typeof test and then calls functions from FileIO class
+    /// to write results of test to file
+    /// </summary>
     public void WriteToFile()
     {
         string output = "";
@@ -286,6 +326,10 @@ public class TestEvalution : MonoBehaviour
         FileIO.instance.WriteToFile(resultsPath, output, false, false);
     }
 
+    /// <summary>
+    /// sets proper strings bases on the typeof test and then calls functions from FileIO class
+    /// to write results of failure to file
+    /// </summary>
     public void WriteToFileFail()
     {
         string output = "";
@@ -320,9 +364,16 @@ public class TestEvalution : MonoBehaviour
 
         FileIO.instance.WriteToFile(resultsPath, output, false, false);
     }
+   
     //for easy test select robot then press o 
     //robot will start rount then after 15 second all commands will be cleared
     //test is complete when robot reached the way point
+
+    /// <summary>
+    /// corountine error for easy nad hard test 
+    /// waits 15 seconds then clears the robots of all commands
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Error()
     {
         if (UIMgr.inst.mTest == TestState.EasyTest1)
@@ -335,13 +386,16 @@ public class TestEvalution : MonoBehaviour
         }
         else if(UIMgr.inst.mTest == TestState.HardTest)
         {
-            Debug.Log("Error for medium started");
+            Debug.Log("Error for hard started");
             yield return new WaitForSeconds(15);
             AIMgr.inst.HandleClear(trackedRobots[0]);
         }
         errorOccured = true;
     }
 
+    /// <summary>
+    /// script for east test sets robot on path towrds waypoint then calles coroutine error 
+    /// </summary>
       public void easyTest1()
     {
         Vector3 testEasy = new Vector3(-19.2f, 18.3f, -16.0f);
@@ -350,6 +404,9 @@ public class TestEvalution : MonoBehaviour
         StartCoroutine(Error());    //wait 15 seconds then do it
     }
 
+    /// <summary>
+    /// sets robots to full speed and error flag to true 
+    /// </summary>
     public void mediumTest()
     {
         //Debug.Log("MediumTest");
@@ -360,12 +417,14 @@ public class TestEvalution : MonoBehaviour
         errorOccured = true;
 
     }
-
+    /// <summary>
+    /// sets both robots to max speed then calls coroutine error 
+    /// </summary>
     public void hardTest()
     {
         //Debug.Log("Hard Test");
-        Vector3 testEasy = new Vector3(0f, 31.11f, -15.25f);
-        AIMgr.inst.HandleMove(trackedRobots[0], testEasy);
+        Vector3 testHard = new Vector3(0f, 31.11f, -15.25f);
+        AIMgr.inst.HandleMove(trackedRobots[0], testHard);
         trackedRobots[1].desiredSpeed = 4.0f;
         StartCoroutine(Error());
         
@@ -373,7 +432,10 @@ public class TestEvalution : MonoBehaviour
 
     }
 
-
+    /// <summary>
+    /// based on the tpe of test check to see if object is complete by checking distance of robot to waypoint 
+    /// </summary>
+    /// <returns></returns> truen when robot is withing 4 units of ways point
     bool checkEnd()
     {
         int counter = 0;
@@ -385,7 +447,7 @@ public class TestEvalution : MonoBehaviour
             {
                 Vector3 targetPos = new Vector3(-19.3f, 18.3f, -15.25f); //position of pink way point 
                 distance = Vector3.Distance(robot.transform.position, targetPos);
-                if(distance < 3.0f)
+                if(distance < 4.0f)
                 {
                     EndTest();
                 }
@@ -410,6 +472,9 @@ public class TestEvalution : MonoBehaviour
         return true;
     }
 
+    /// <summary>
+    /// pauses game and shows hint pannel
+    /// </summary>
     public void hint()
     {
         Debug.Log("hint");
@@ -431,7 +496,9 @@ public class TestEvalution : MonoBehaviour
         
     }
 
-
+    /// <summary>
+    /// calls UpdateRobotPositionTracking() updates totaltime and check to see if the test has ended 
+    /// </summary>
     private void Update()
     {
         UpdateRobotPositionTracking();
